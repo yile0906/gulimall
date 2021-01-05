170dcc2f9f279618eca8c2b0688c0010f3a50420
ckxlovezqq@163.com <ckxlovezqq@163.com>
2021-01-05 15:00:24 +0800

    openFegin使用方法备注

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
@@ -6,7 +6,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 这是声明式的远程电泳
+ * 这是声明式的远程调用
+ *  1.引入依赖
+ *      <dependency>
+ *             <groupId>org.springframework.cloud</groupId>
+ *             <artifactId>spring-cloud-starter-openfeign</artifactId>
+ *         </dependency>
+ *  2.调用方启动类加上注解@EnableFeignClients
+ *  3.定义接口并且指定需要调用的应用名称 application.name      例：@FeignClient("gulimall-coupon")
+ *  4.调用的方法的全限定名和路径(直接复制就行)   例子：@RequestMapping("/coupon/coupon/member/list")
  */
 @FeignClient("gulimall-coupon")
 public interface CouponFeignService {
