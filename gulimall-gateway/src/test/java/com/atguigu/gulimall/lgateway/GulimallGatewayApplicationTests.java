165e9cd4d04e6b417270c27ffb90c5fda74b9783
ckxlovezqq@163.com <ckxlovezqq@163.com>
2021-01-05 16:12:06 +0800

    springcloud-gateway网关

---
+++ gulimall-gateway/src/test/java/com/atguigu/gulimall/lgateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.atguigu.gulimall.lgateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
